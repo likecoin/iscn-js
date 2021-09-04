5dcb3d44b1a7c6f74059bd7ac2e2fc492a9731d0
William Chong <me@williamchong.cloud>
2021-09-05 01:34:19 +0800

    ✨ Add ISCN query client

---
+++ src/types.d.ts
@@ -0,0 +1,35 @@
+export interface ISCNRecordData {
+  stakeholders: any[];
+  contentMetadata: any;
+  recordNotes: string;
+  contentFingerprints: string[];
+}
+
+export interface ISCNRecord {
+  ipld: string;
+  data: ISCNRecordData;
+}
+
+export interface Entity {
+  '@id': string;
+  name?: string;
+  url?: string;
+}
+
+export interface Stakeholder {
+  entity: Entity,
+  rewardProportion: number,
+  contributionType: string
+}
+
+export interface ISCNSignPayload {
+  name: string;
+  description: string;
+  keywords: string[];
+  url: string;
+  contentFingerprints: string[];
+  stakeholders: Stakeholder[];
+  type: string;
+  usageInfo: string;
+  recordNotes: string;
+}

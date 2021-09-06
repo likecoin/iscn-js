e9b2a490765f03969341e1ad6fc0ceefc847e852
William Chong <me@williamchong.cloud>
2021-09-06 23:19:56 +0800

    ✨ Add query by tx ID

---
+++ src/parsing.ts
@@ -0,0 +1,86 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { decodeTxRaw } from '@cosmjs/proto-signing';
+import { IndexedTx } from '@cosmjs/stargate';
+import { QueryResponseRecord } from '@likecoin/iscn-message-types/dist/iscn/query';
+import {
+  IscnRecord,
+  MsgCreateIscnRecord,
+  MsgUpdateIscnRecord,
+  MsgChangeIscnRecordOwnership,
+} from '@likecoin/iscn-message-types/dist/iscn/tx';
+import { ISCNRecord, ISCNRecordData } from './types';
+
+export function parseISCNRecordFields(record: IscnRecord): ISCNRecordData {
+  const {
+    stakeholders,
+    contentMetadata,
+  } = record;
+  return {
+    ...record,
+    stakeholders: stakeholders.map((s: Uint8Array) => {
+      if (s) {
+        const sString = Buffer.from(s).toString('utf-8');
+        if (sString) return JSON.parse(sString);
+      }
+      return s;
+    }),
+    contentMetadata: JSON.parse(Buffer.from(contentMetadata).toString('utf-8')),
+  };
+}
+
+export function parseISCNTxInfoFromIndexedTx(tx: IndexedTx) {
+  const { tx: txBytes, rawLog } = tx;
+  const decodedTx = decodeTxRaw(txBytes);
+  const messages = decodedTx.body.messages.map((m) => {
+    if (m.typeUrl === '/likechain.iscn.MsgCreateIscnRecord') {
+      const msg = MsgCreateIscnRecord.decode(m.value);
+      if (msg.record) {
+        msg.record = parseISCNRecordFields(msg.record);
+      }
+      return {
+        ...m,
+        value: msg,
+      };
+    }
+    if (m.typeUrl === '/likechain.iscn.MsgUpdateIscnRecord') {
+      const msg = MsgUpdateIscnRecord.decode(m.value);
+      if (msg.record) {
+        msg.record = parseISCNRecordFields(msg.record);
+      }
+      return {
+        ...m,
+        value: msg,
+      };
+    }
+    if (m.typeUrl === '/likechain.iscn.MsgChangeIscnRecordOwnership') {
+      const msg = MsgChangeIscnRecordOwnership.decode(m.value);
+      return {
+        ...m,
+        value: msg,
+      };
+    }
+    return null;
+  });
+  return {
+    ...tx,
+    logs: JSON.parse(rawLog),
+    tx: {
+      ...decodedTx,
+      body: {
+        ...decodedTx.body,
+        messages: messages.filter((m) => !!m),
+      },
+    },
+  };
+}
+
+export function parseISCNTxRecordFromQuery(records: QueryResponseRecord[]) {
+  return records.map((r): ISCNRecord => {
+    const { data, ipld } = r;
+    const parsedData = JSON.parse(Buffer.from(data).toString('utf-8'));
+    return {
+      ipld,
+      data: parsedData,
+    };
+  });
+}

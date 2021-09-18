e4641fba612f8a6ee10c953d58dc4d242c17328a
William Chong <me@williamchong.cloud>
2021-09-18 13:18:00 +0800

    ✅ Add large payload test for gas estimation

--- src/signingClient.spec.ts
+++ src/signingClient.spec.ts
@@ -1,16 +1,27 @@
 import { ISCNSigningClient } from './signingClient';
-import testData from './tests/iscn-sample-1.json';
+import testData1 from './tests/iscn-sample-1.json';
+import testData2 from './tests/iscn-sample-2.json';
 
 describe('queryClient', () => {
   const client = new ISCNSigningClient();
 
   test('Estimate ISCN gas and fee', async () => {
-    const res = await client.esimateISCNTxGasAndFee(testData);
+    const res = await client.esimateISCNTxGasAndFee(testData1);
     expect(res).toEqual(
-      expect.objectContaining({ gas: expect.objectContaining({ fee: expect.objectContaining({ gas: '185878' }) }) }),
+      expect.objectContaining({ gas: expect.objectContaining({ fee: expect.objectContaining({ gas: '169847' }) }) }),
     );
     expect(res).toEqual(
       expect.objectContaining({ iscnFee: expect.objectContaining({ amount: '2744000' }) }),
+    );
+  });
+
+  test('Estimate large ISCN gas and fee', async () => {
+    const res = await client.esimateISCNTxGasAndFee(testData2);
+    expect(res).toEqual(
+      expect.objectContaining({ gas: expect.objectContaining({ fee: expect.objectContaining({ gas: '248124' }) }) }),
+    );
+    expect(res).toEqual(
+      expect.objectContaining({ iscnFee: expect.objectContaining({ amount: '5848000' }) }),
     );
   });
 });

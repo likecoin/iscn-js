c5fbe999fc4a9f8c6118e553bb3306dd8f95e16b
William Chong <me@williamchong.cloud>
2021-09-21 13:35:09 +0800

    ✅ Add custom gasPrice testcase

--- src/signingClient.spec.ts
+++ src/signingClient.spec.ts
@@ -8,7 +8,14 @@ describe('queryClient', () => {
   test('Estimate ISCN gas and fee', async () => {
     const res = await client.esimateISCNTxGasAndFee(testData1);
     expect(res).toEqual(
-      expect.objectContaining({ gas: expect.objectContaining({ fee: expect.objectContaining({ gas: '169847' }) }) }),
+      expect.objectContaining({
+        gas: expect.objectContaining({
+          fee: expect.objectContaining({
+            gas: '169847',
+            amount: expect.arrayContaining([expect.objectContaining({ amount: '169847000' })]),
+          }),
+        }),
+      }),
     );
     expect(res).toEqual(
       expect.objectContaining({ iscnFee: expect.objectContaining({ amount: '2744000' }) }),
@@ -22,6 +29,24 @@ describe('queryClient', () => {
     );
     expect(res).toEqual(
       expect.objectContaining({ iscnFee: expect.objectContaining({ amount: '5848000' }) }),
+    );
+  });
+
+
+  test('Estimate ISCN gas with custom gas price', async () => {
+    const res = await client.esimateISCNTxGasAndFee(testData1, { gasPrice: 1 });
+    expect(res).toEqual(
+      expect.objectContaining({
+        gas: expect.objectContaining({
+          fee: expect.objectContaining({
+            gas: '169847',
+            amount: expect.arrayContaining([expect.objectContaining({ amount: '169847' })]),
+          }),
+        }),
+      }),
+    );
+    expect(res).toEqual(
+      expect.objectContaining({ iscnFee: expect.objectContaining({ amount: '2744000' }) }),
     );
   });
 });

f63cc41dc71c8dcf756f74dd0bdf2d4b3fd1c5f0
William Chong <me@williamchong.cloud>
2021-09-21 16:30:18 +0800

    ✅ Add custom sign param testcase

--- src/signingClient.spec.ts
+++ src/signingClient.spec.ts
@@ -1,11 +1,31 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { DirectSecp256k1HdWallet, AccountData } from '@cosmjs/proto-signing';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
 import { ISCNSigningClient } from './signingClient';
+import { computeTransactionHash } from './tests/utils';
+import { mnemonic } from './tests/key.json';
 import testData1 from './tests/iscn-sample-1.json';
 import testData2 from './tests/iscn-sample-2.json';
 
-describe('queryClient', () => {
-  const client = new ISCNSigningClient();
+let signingClient: ISCNSigningClient | undefined;
+let signingWallet: AccountData | undefined;
 
+async function getSigner() {
+  if (!signingWallet || !signingClient) {
+    const client = new ISCNSigningClient();
+    const signer = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
+    await client.connectWithSigner('https://mainnet-node.like.co/rpc/', signer);
+    const [wallet] = await signer.getAccounts();
+    signingClient = client;
+    signingWallet = wallet;
+  }
+  return { client: signingClient, wallet: signingWallet };
+}
+
+describe('queryClient', () => {
   test('Estimate ISCN gas and fee', async () => {
+    const { client } = await getSigner();
     const res = await client.esimateISCNTxGasAndFee(testData1);
     expect(res).toEqual(
       expect.objectContaining({
@@ -23,6 +43,7 @@ describe('queryClient', () => {
   });
 
   test('Estimate large ISCN gas and fee', async () => {
+    const { client } = await getSigner();
     const res = await client.esimateISCNTxGasAndFee(testData2);
     expect(res).toEqual(
       expect.objectContaining({ gas: expect.objectContaining({ fee: expect.objectContaining({ gas: '248124' }) }) }),
@@ -32,8 +53,8 @@ describe('queryClient', () => {
     );
   });
 
-
   test('Estimate ISCN gas with custom gas price', async () => {
+    const { client } = await getSigner();
     const res = await client.esimateISCNTxGasAndFee(testData1, { gasPrice: 1 });
     expect(res).toEqual(
       expect.objectContaining({
@@ -48,5 +69,23 @@ describe('queryClient', () => {
     expect(res).toEqual(
       expect.objectContaining({ iscnFee: expect.objectContaining({ amount: '2744000' }) }),
     );
+  });
+
+  test('Sign ISCN create record with sign param', async () => {
+    const { client, wallet } = await getSigner();
+    const signedTxRaw = await client.createISCNRecord(
+      wallet.address, testData1, {
+        broadcast: false, sequence: 1, accountNumber: 0, chainId: 'likecoin-mainnet-2',
+      },
+    );
+    const hash = await computeTransactionHash(signedTxRaw as TxRaw);
+    expect(hash).toEqual('A8D6C7CAC38DA365DBB0FF89991023BB228FD2E765948F7A4CE3635F2A94380C');
+    const signedTxRaw2 = await client.createISCNRecord(
+      wallet.address, testData1, {
+        broadcast: false, sequence: 2, accountNumber: 0, chainId: 'likecoin-mainnet-2',
+      },
+    );
+    const hash2 = await computeTransactionHash(signedTxRaw2 as TxRaw);
+    expect(hash2).toEqual('92849D1263EE386896AB21B389BD7CD76D6D67F750BA6CC18FEDE31919C92039');
   });
 });
---
+++ src/tests/utils.ts
@@ -0,0 +1,14 @@
+import { createHash } from 'crypto';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
+
+export async function computeTransactionHash(signed: TxRaw): Promise<string> {
+  const tx = Uint8Array.from(TxRaw.encode(signed).finish());
+  const sha256 = createHash('sha256');
+  const txHash = sha256
+    .update(Buffer.from(tx.buffer))
+    .digest('hex');
+  return txHash.toUpperCase();
+}
+
+export default computeTransactionHash;

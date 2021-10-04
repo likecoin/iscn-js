fb8defad8bb7b782fdee17a9279ec1bf2682f874
AuroraHuang22 <aurora90376@gmail.com>
2021-10-04 19:12:35 +0800

    ✨ Add feature to submit all keys from the payload

--- src/signingClient.ts
+++ src/signingClient.ts
@@ -50,6 +50,7 @@ export function formatISCNPayload(payload: ISCNSignPayload, version = 1) {
     type,
     usageInfo,
     recordNotes,
+    ...data
   } = payload;
 
   const stakeholders = inputStakeHolders.map((s: Stakeholder) => Buffer.from(
@@ -65,6 +66,7 @@ export function formatISCNPayload(payload: ISCNSignPayload, version = 1) {
     url,
     keywords: keywords.join(','),
     usageInfo,
+    ...data,
   };
   return {
     recordNotes,

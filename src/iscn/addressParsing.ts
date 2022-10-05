6541ce3bf85ed0d9ff86b34e9b7dea8a4a309016
hsueh kuan tsai <b123456785678@gmail.com>
2022-10-05 18:02:50 +0800

    ♻️ Need approve undefined input

--- src/iscn/addressParsing.ts
+++ src/iscn/addressParsing.ts
@@ -14,16 +14,16 @@ export function changeAddressPrefix(address: string, newPrefix: string): string 
   return bech32.encode(newPrefix, words);
 }
 
-export function getLikeWalletAddress(id: string): string | null {
+export function getLikeWalletAddress(id: string | undefined): string | null {
   const addressLengthWithoutPrefixAnd1 = 38;
   let add = id;
   let likeWallet: string | null = null;
-  if (id.startsWith('did:like:')) {
+  if (id && id.startsWith('did:like:')) {
     add = `like1${id.slice(id.length - addressLengthWithoutPrefixAnd1)}`;
-  } else if (id.startsWith('did:cosmos:')) {
+  } else if (id && id.startsWith('did:cosmos:')) {
     add = `cosmos1${id.slice(id.length - addressLengthWithoutPrefixAnd1)}`;
   }
-  if (isValidAddress(add)) {
+  if (add && isValidAddress(add)) {
     likeWallet = changeAddressPrefix(add, 'like');
   }
   return likeWallet;

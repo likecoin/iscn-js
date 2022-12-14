685c0012fda3b6113721e7af2f1b01e69f1ce28c
William Chong <me@williamchong.cloud>
2022-12-14 20:14:57 +0800

    ✅ Add simple NFT market test case

--- src/tests/constant.ts
+++ src/tests/constant.ts
@@ -1,3 +1,5 @@
 export const ISCN_ID = 'iscn://likecoin-chain/dLbKMa8EVO9RF4UmoWKk2ocUq7IsxMcnQL1_Ps5Vg80/1';
 
 export const NFT_CLASS_ID = 'likenft1c86xz87908m90pl7c6ygulyaxcccsdgy9f7rutxg07xe24wvuksswzukgw';
+
+export const NFT_ID = 'testing-321321';
